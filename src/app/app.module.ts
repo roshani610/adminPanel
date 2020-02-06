7a8ddc4aeb8f9f6de35aa3b5c3138947e6f4e056
roshani610 <shahroshani.v@gmail.com>
2020-02-06 12:25:00 +0530

    basic layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin/layout/header/header.component';
+import { SidemenuComponent } from './admin/layout/sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
